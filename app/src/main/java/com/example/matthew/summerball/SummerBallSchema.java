68e5a2d5ad31dbd6dc771f95281e4a796b58121b
Matt Bacher <mattjb1991@sbcglobal.net>
2018-06-06 21:48:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/matthew/summerball/SummerBallSchema.java
@@ -0,0 +1,17 @@
+package com.example.matthew.summerball;
+
+public class SummerBallSchema {
+    public static final class SummerBallTable {
+
+
+        public static final String Name = "Customer";
+
+        public static final class SummerBallColumns {
+            public static final String CustomerUID = "CustomerUID";
+            public static final String FristName = "FirstName";
+            public static final String LastName = "LastName";
+            public static final String Email = "Email";
+
+        }
+    }
+}
